44898bfefbceb66b64ddac4f30b5591df26685e2
rebelBeard <rfm144@gmail.com>
2017-02-23 15:36:13 -0500

    optimized images

--- js/portfolio.js
+++ js/portfolio.js
@@ -12,13 +12,13 @@ let row = document.querySelector('.row');
 
 // array of objects containing the data to build each card quickly
 const projectsObj = [
-	{
-		title: 'Tribute Page',
-		description: 'This page is a tribute to chess legend, Bobby Fischer.',
-		tools: 'HTML - CSS - Responsive Design',
-		link: 'projects/bobby_fischer/bobby.html',
-		name: 'bobby'
-	},
+	// {
+	// 	title: 'Tribute Page',
+	// 	description: 'This page is a tribute to chess legend, Bobby Fischer.',
+	// 	tools: 'HTML - CSS - Responsive Design',
+	// 	link: 'projects/bobby_fischer/bobby.html',
+	// 	name: 'bobby'
+	// },
 	{
 		title: 'Random Quote Generator',
 		description: 'A random quote machine for the TV Show Silicon Valley',
